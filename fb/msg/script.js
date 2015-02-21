c91af402a05d2a126cb7b1eda9dc0de49a373a74
Zack Guard <zacharyguard@gmail.com>
2015-02-21 17:59:40 +0800

    Show images for 1:1 chats

--- fb/msg/script.js
+++ fb/msg/script.js
@@ -11,6 +11,8 @@ var main = function(){
             if (datum.comments) {
                 var dateString = new Date(datum.updated_time).toLocaleTimeString();
                 var imgSrc = "//placehold.it/50x50";
+                
+                var chatID = datum.id;
 
                 var contentString = "";
                 var lastMessage = datum.comments.data[datum.comments.data.length - 1];
@@ -21,13 +23,22 @@ var main = function(){
                 }
 
                 var membersArray = [];
+                var idsArray = [];
                 datum.to.data.forEach(function(toNode){
                     if (toNode.id !== USER_ID) {
                         membersArray.push(toNode.name);
+                        idsArray.push(toNode.id);
                     }
                 });
+                
+                if (idsArray.length === 1) {
+                    FB.api(idsArray[0] + "/picture", function(r){
+                        var newSrc = r.data.url;
+                        document.querySelector(".list-item[data-chat-id='" + chatID + "'] .item-img img").src = newSrc;
+                    });
+                }
 
-                msgsList.appendChild(makeListItem(membersArray.join(", "), contentString, imgSrc, dateString));
+                msgsList.appendChild(makeListItem(membersArray.join(", "), contentString, imgSrc, dateString, chatID));
             }
         });
     });
