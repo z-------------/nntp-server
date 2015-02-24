03e08c0ca3268ac1194af9c88779a61531f14d01
Zack Guard <zacharyguard@gmail.com>
2015-02-24 21:14:28 +0800

    wx stuff

--- wx/index.php
+++ wx/index.php
@@ -13,7 +13,7 @@ $ipAPIData = json_decode($ipAPIResponse, TRUE);
 $latLngStr = $ipAPIData["geobyteslatitude"] . "," . $ipAPIData["geobyteslongitude"];
 
 $wxYQLQuery = "set api_key='2319d1510ebae6b2d61d69ec6dc6ac14' on flickr.places; select * from weather.woeid where w in (select place.woeid from flickr.places(1) where (lat,lon) in (" . $latLngStr . ")) and u='c'";
-$wxAPIURL = "https://query.yahooapis.com/v1/public/yql?q=" . urlencode($wxYQLQuery) . "&format=json";
+$wxAPIURL = "https://query.yahooapis.com/v1/public/yql?q=" . urlencode($wxYQLQuery) . "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 $wxAPIResponse = file_get_contents($wxAPIURL);
 $wxAPIData = json_decode($wxAPIResponse, TRUE);
 
