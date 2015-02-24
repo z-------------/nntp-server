ce2d31654ebd8ed60a8294ad3d6d4aba29c616f8
Zack Guard <zacharyguard@gmail.com>
2015-02-24 20:43:26 +0800

    Weather stuff

--- wx/index.php
+++ wx/index.php
@@ -1,20 +1,22 @@
 <?php
 
-$ip = $_SERVER['REMOTE_ADDR'];
+$ip = $_SERVER["REMOTE_ADDR"];
 
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
+    $ip = $_SERVER["HTTP_CLIENT_IP"];
+} elseif (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+    $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
 }
 
 $API_KEY = "f23a8d5803e02eb71baa30cc2ab0c2a8";
-$API_URL = "https://api.forecast.io/forecast/" . $API_KEY . "/";
+$apiURL = "https://api.forecast.io/forecast/" . $API_KEY . "/";
 
-$IP_API_URL = "http://gd.geobytes.com/GetCityDetails?fqcn=" . $ip;
+$ipApiURL = "http://gd.geobytes.com/GetCityDetails?fqcn=" . $ip;
 
-$ipAPIResponse = file_get_contents($IP_API_URL);
+$ipAPIResponse = file_get_contents($ipApiURL);
+$ipAPIData = json_decode($ipAPIResponse);
 
 print($ip);
+print($ipAPIResponse);
 
 ?>
