86f132e0bb06c333d5cb871cb345b825e74aa653
Zack Guard <zacharyguard@gmail.com>
2015-03-15 00:35:07 +0800

    Fix weather thinga

--- wx/index.php
+++ wx/index.php
@@ -1,18 +1,11 @@
 <?php
 
-$ip = $_SERVER["REMOTE_ADDR"];
-if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
-    $ip = $_SERVER["HTTP_CLIENT_IP"];
-} elseif (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
-    $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
-}
-
-$ipAPIURL = "http://gd.geobytes.com/GetCityDetails?fqcn=" . $ip;
+$ipAPIURL = "http://ipinfo.io/geo";
 $ipAPIResponse = file_get_contents($ipAPIURL);
 $ipAPIData = json_decode($ipAPIResponse, TRUE);
-$latLngStr = $ipAPIData["geobyteslatitude"] . "," . $ipAPIData["geobyteslongitude"];
+$latLngStr = $ipAPIData["loc"];
 
-$wxYQLQuery = "set api_key='2319d1510ebae6b2d61d69ec6dc6ac14' on flickr.places; select * from weather.woeid where w in (select place.woeid from flickr.places(1) where (lat,lon) in (" . $latLngStr . ")) and u='c'";
+$wxYQLQuery = "select * from weather.forecast where woeid in (SELECT woeid FROM geo.placefinder WHERE text='" . $latLngStr . "' and gflags='R')";
 $wxAPIURL = "https://query.yahooapis.com/v1/public/yql?q=" . urlencode($wxYQLQuery) . "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 $wxAPIResponse = file_get_contents($wxAPIURL);
 
