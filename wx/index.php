02c3184ba2d90e55e7e4a5a8048a42786c9674dc
Zack Guard <zacharyguard@gmail.com>
2015-03-15 12:07:36 +0800

    Prioritise ip in get variable

--- wx/index.php
+++ wx/index.php
@@ -1,7 +1,9 @@
 <?php
 
 $ip = $_SERVER["REMOTE_ADDR"];
-if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
+if (!empty($_GET["ip"])) {
+    $ip = $_GET["ip"];
+} elseif (!empty($_SERVER["HTTP_CLIENT_IP"])) {
     $ip = $_SERVER["HTTP_CLIENT_IP"];
 } elseif (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
     $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
