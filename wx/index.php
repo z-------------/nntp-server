71523e6dbf8cafe9791d37e2c03692cd48bc6556
Zack Guard <zacharyguard@gmail.com>
2015-03-15 12:22:59 +0800

    Add debug info to JSON output

--- wx/index.php
+++ wx/index.php
@@ -19,11 +19,21 @@ $ipAPIResponse = file_get_contents($ipAPIURL);
 $ipAPIData = json_decode($ipAPIResponse, TRUE);
 $latLngStr = $ipAPIData["loc"];
 
-$wxYQLQuery = "select * from weather.forecast where woeid in (SELECT woeid FROM geo.placefinder WHERE text='" . $latLngStr . "' and gflags='R')";
+$wxYQLQuery = "SELECT item FROM weather.forecast WHERE woeid IN (SELECT woeid FROM geo.placefinder WHERE text='" . $latLngStr . "' AND gflags='R')";
 $wxAPIURL = "https://query.yahooapis.com/v1/public/yql?q=" . urlencode($wxYQLQuery) . "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 $wxAPIResponse = file_get_contents($wxAPIURL);
 
+$response = json_decode($wxAPIResponse);
+$response->debug = array(
+    "ip" => $ip,
+    "yql_query" => $wxYQLQuery,
+    "ipinfo_url" => $ipAPIURL
+);
+
+$response = json_encode($response, JSON_PRETTY_PRINT);
+
 header("Content-Type: application/json;charset=utf-8");
-print($ip . $wxAPIResponse);
+
+print($response);
 
 ?>
